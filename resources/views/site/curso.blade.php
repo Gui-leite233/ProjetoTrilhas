37ab4bdcf5ef731f14d297bdde40140c9ac881be
guilherme <89623564+Gui-leite233@users.noreply.github.com>
2024-10-02 21:41:01 -0300

    TCC

--- resources/views/site/curso.blade.php
+++ resources/views/site/curso.blade.php
@@ -4,4 +4,18 @@
 
 @section('conteudo')
 
+<div class="row mb-3">
+    <div class="col">
+        <div class="accordion accordion-flush" id="accordionFlushExample">
+            @foreach ($data as $item)
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="flush-headingOne">
+                    <span class="text-success fs-10">Curso: {{ $item->nome }}</span>
+                    </h2>
+                </div>
+            @endforeach
+        </div>
+    </div>
+</div>
+
 @endsection
