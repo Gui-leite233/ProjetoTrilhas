8b082db07c655a6fe1d5bea3c7e192b97da223e9
Gui-leite233 <89623564+Gui-leite233@users.noreply.github.com>
2024-12-09 00:14:22 -0300

    mais Otimizações

--- app/Http/Controllers/CursoController.php
+++ app/Http/Controllers/CursoController.php
@@ -7,116 +7,67 @@ use App\Models\Curso;
 
 class CursoController extends Controller
 {
-    /**
-     * Display a listing of the resource. 
-     */
+
+    private $regras = [
+        'nome' => 'required|max:100|min:10',
+        'descricao' => 'required|max:1000|min:20',
+    ];
+
+    private $mensagens = [
+        "required" => "O preenchimento do campo :attribute é obrigatório!",
+        "max" => "O campo :attribute possui tamanho máximo de :max caracteres!",
+        "min" => "O campo :attribute possui tamanho mínimo de :min caracteres!",
+    ];
+
+
     public function index()
     {
         $data = Curso::orderBy('nome')->get();
         return view('curso.index', compact('data'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
+
     public function create()
     {
         return view('curso.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+
     public function store(Request $request)
     {
-        $regras = [
-            'nome' => 'required|max:100|min:10',
-            'descricao' => 'required|max:1000|min:20',
-        ];
+        $request->validate($this->regras, $this->mensagens);
 
-        $msgs = [
-            "required" => "O preenchimento do campo [:attribute] é obrigatório!",
-            "max" => "O campo [:attribute] possui tamanho máximo de [:max] caracteres!",
-            "min" => "O campo [:attribute] possui tamanho mínimo de [:min] caracteres!",
-        ];
-
-        $request->validate($regras, $msgs);
-
-        // Insert no Banco
-        $reg = new Curso();
-        $reg->nome = $request->nome;
-        $reg->descricao = $request->descricao;
-        $reg->save();
+        Curso::create($request->only(['nome', 'descricao']));
 
         return redirect()->route('curso.index');
     }
 
 
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(string $id)
     {
-        $dados = Curso::find($id);
-
-        if (isset($dados)) {
-            return view('curso.edit', compact('dados'));
-        }
+        $dados = Curso::findOrFail($id);
+        return view('curso.edit', compact('dados'));
     }
 
     /**
-     * Update the specified resource in storage.
+     * Atualiza um recurso específico no banco de dados.
      */
     public function update(Request $request, string $id)
     {
-        $obj = Curso::find($id);
+        $obj = Curso::findOrFail($id);
+        
+        $request->validate($this->regras, $this->mensagens);
 
-        if (!isset($obj)) {
-            return "<h1>ID: $id não encontrado!</h1>";
-        }
-
-        $regras = [
-            'nome' => 'required|max:100|min:10',
-            'descricao' => 'required|max:1000|min:20'
-        ];
-
-        $msgs = [
-            "required" => "O preenchimento do campo :attribute é obrigatório!",
-            "max" => "O campo :attribute possui tamanho máximo de [:max] caracteres!",
-            "min" => "O campo :attribute possui tamanho mínimo de [:min] caracteres!",
-        ];
-
-        $request->validate($regras, $msgs);
-
-        $obj->nome = $request->nome;
-        $obj->descricao = $request->descricao;
-        $obj->save();
+        $obj->update($request->only(['nome', 'descricao']));
 
         return redirect()->route('curso.index');
     }
 
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
-        $obj = Curso::find($id);
-
-        if (!isset($obj)) {
-            return "<h1>ID: $id não encontrado!";
-        }
-
-        $obj->destroy($id);
+        $obj = Curso::findOrFail($id);
+        $obj->delete();
         return redirect()->route('curso.index');
     }
 }
